5f31751cb273cbccfcecfc85968febaa4f716f7b
emoren <3210103568@zju.edu.cn>
2024-05-05 18:13:52 +0800

    delete history

--- .history/post_online_20240505000901.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:5000/submit' 
-files = {'file': ('Upload_test_file.txt', open('Upload_test_file.txt', 'rb'), 'text/plain')}  # 要上传的文件
-
-response = requests.post(url, files=files) 
--- .history/post_online_20240505003720.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'https://1mocat.github.io/'  # Flask应用的URL
-files = {'file': ('Upload_test_file.txt', open('Upload_test_file.txt', 'rb'), 'text/plain')}  # 要上传的文件
-data = {'name': 'maoren', 'email': 'maoren@example.com'}  # 其他表单数据
-
-response = requests.post(url, files=files, data=data)  # 发送POST请求
-
-print(response.text)  # 打印服务器响应的内容
